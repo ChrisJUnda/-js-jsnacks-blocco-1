49cbb428d1211287912b214cab43faede046d795
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-05-07 01:45:29 +0200

    [UPD] Jssnack 9 done

---
+++ jssnack9/js/main.js
@@ -0,0 +1,29 @@
+'use strict'
+
+
+
+
+
+
+
+
+
+
+
+
+let sum = 0;
+let avg = 0;
+
+for(let value = 1; value <= 10; value++){
+    sum += value;
+}
+
+avg = sum / 10;
+
+
+
+
+
+
+
+console.log(`la somma è ${sum} e la media è ${avg}`);
